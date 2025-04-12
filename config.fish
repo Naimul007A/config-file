12b624673593499d031c25a7804f1e7d81fc06c3
Naimul Islam <112851392+Naimul007A@users.noreply.github.com>
2025-04-12 10:10:39 +0600

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,21 +1,45 @@
+set -g fish_greeting
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-starship init fish | source
-export PATH="$PATH:$HOME/.config/composer/vendor/bin"
-alias switchphp="sudo update-alternatives --config php"
+    starship init fish | source
+end
+
+# List Directory
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+alias py-init='python3 -m venv .venv'
+alias py-a='. .venv/bin/activate.fish'
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
 alias pa="php artisan"
-alias update="sudo apt-get update && sudo apt-get upgrade"
-alias reboot="sudo reboot now"
-alias shutdown="sudo shutdown now"
-end
-set -x PHPENV_ROOT "/home/naimul/.phpenv"
-if test -d "/home/naimul/.phpenv"
-  set -x PATH "/home/naimul/.phpenv/bin" $PATH
-  status --is-interactive; and . (phpenv init -|psub)
-end
+export PATH="$PATH:$HOME/.config/composer/vendor/bin"
+alias update="sudo pacman -Syu && yay"
+
 function ph
    # set dir (or $argv .)  # If an argument is passed, use it; otherwise, use the current directory (.)
     phpstorm $argv > /dev/null 2>&1 &
+end
+function pc
+   # set dir (or $argv .)  # If an argument is passed, use it; otherwise, use the current directory (.)
+    pycharm-professional $argv > /dev/null 2>&1 &
+end
+function cr
+   # set dir (or $argv .)  # If an argument is passed, use it; otherwise, use the current directory (.)
+    cursor $argv > /dev/null 2>&1 &
+end
+function ws
+   # set dir (or $argv .)  # If an argument is passed, use it; otherwise, use the current directory (.)
+    webstorm $argv > /dev/null 2>&1 &
 end
 
 # pnpm
@@ -28,6 +52,9 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-#debian message
-set -x BASH_SILENCE_DEBIAN_MESSAGE 1
-#fnm env --use-on-cd --shell fish | source
+
+# bore.pub
+export PATH="$HOME/.cargo/bin:$PATH"
+
+#idea.helper
+alias idea-healper="php artisan ide-helper:generate && php artisan ide-helper:models --nowrite  && php artisan ide-helper:meta"
